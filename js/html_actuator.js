06f1890a16964801e1d13bc6cddb248363e35ee9
Cherry NG <cherry92@gmail.com>
2014-03-27 17:42:07 +0800

    no more text and color to differentiate different numbered tile

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -55,14 +55,16 @@ HTMLActuator.prototype.addTile = function (tile) {
   var positionClass = this.positionClass(position);
 
   // We can't use classlist because it somehow glitches when replacing classes
-  var classes = ["tile", "tile-" + tile.value, positionClass];
+  // var classes = ["tile", "tile-" + tile.value, positionClass];
+  var classes = ["tile", "tile-2", positionClass];
 
   if (tile.value > 2048) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  // inner.textContent = tile.value;
+  inner.textContent = '';
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
